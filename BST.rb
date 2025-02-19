617c9720538d237b6902db5e6befd981cfe5243e
Dhinakaran <dhinaharanmohan79@gmail.com>
2025-02-19 15:53:50 +0530

    I have change the program so it contains only one class and imported the Node class in it

---
+++ BST.rb
@@ -0,0 +1,68 @@
+
+class TreeNode
+  attr_accessor :data_part, :left_child, :right_child
+
+  def initialize(data_part)
+    @data_part=data_part
+    @left_child=nil
+    @right_child=nil
+  end
+end
+
+class BST
+  attr_accessor :root_node
+  def initialize
+    @root_node=nil
+  end
+
+  def insert_node(data_part)
+    if @root_node.nil?
+      @root_node=TreeNode.new(data_part)
+    else
+      current_node=@root_node
+      while true
+        if current_node.nil?
+          current_node=TreeNode.new(data_part)
+          break
+        end
+        if current_node.data_part<data_part
+          if current_node.right_child.nil?
+           current_node.right_child=TreeNode.new(data_part)
+            break
+          else
+          current_node=current_node.right_child
+          next
+          end
+        end
+        if current_node.data_part>data_part
+          if current_node.left_child.nil?
+            current_node.left_child=TreeNode.new(data_part)
+            break
+          else
+          current_node=current_node.left_child
+          next
+          end
+        end
+      end
+    end
+  end
+
+  def inorder_traversal(temp_root)
+    if temp_root.nil?
+      return
+    end
+    inorder_traversal(temp_root.left_child)
+    puts(temp_root.data_part)
+    inorder_traversal(temp_root.right_child)
+  end
+end
+
+r1=BST.new()
+r1.insert_node(90)
+r1.insert_node(89)
+r1.insert_node(11)
+r1.insert_node(2)
+r1.insert_node(6)
+r1.insert_node(9)
+r1.inorder_traversal(r1.root_node)
+
--- LinkedList.rb
+++ LinkedList.rb
@@ -1,11 +1,36 @@
-class Node
-  @head=nil
-  attr_accessor:data,:next
-  def initialize(data)
-    @data=data
-    @next=nil
+require_relative 'Node'
+class LinkedList
+  def initialize
+    @head_node=nil
   end
+
+  def insert(data_part)
+    if @head_node.nil?
+      @head_node=Node.new(data_part)
+    else
+      current_node=@head_node
+      while current_node.next_pointer != nil
+        current_node=current_node.next_pointer
+      end
+      current_node.next_pointer=Node.new(data_part)
+  end
+  end
+
+  def display
+    temp_node=@head_node
+    while temp_node != nil
+      puts temp_node.data_part
+      temp_node=temp_node.next_pointer
+    end
+  end
+
 end
 
-First=Node.new(1000);
+l1=LinkedList.new()
+l1.insert(5)
+l1.insert(9)
+l1.insert(11)
+l1.insert(6)
+l1.display
+
 
