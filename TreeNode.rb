0bcabb797ccfb395393741e9c7ed785e457f3bb6
Dhinakaran <dhinaharanmohan79@gmail.com>
2025-02-19 15:57:46 +0530

    This is the tree node which contains properties of a node of a tree data structure

---
+++ TreeNode.rb
@@ -0,0 +1,10 @@
+class TreeNode
+  attr_accessor :data_part, :left_child, :right_child
+
+  def initialize(data_part)
+    @data_part=data_part
+    @left_child=nil
+    @right_child=nil
+  end
+end
+
