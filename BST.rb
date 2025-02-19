8b3b05da4b72fde29a6b56eae97e9f07451101b9
Dhinakaran <dhinaharanmohan79@gmail.com>
2025-02-19 16:02:17 +0530

    My First Task

--- BST.rb
+++ BST.rb
@@ -1,13 +1,4 @@
-
-class TreeNode
-  attr_accessor :data_part, :left_child, :right_child
-
-  def initialize(data_part)
-    @data_part=data_part
-    @left_child=nil
-    @right_child=nil
-  end
-end
+require_relative 'TreeNode'
 
 class BST
   attr_accessor :root_node
