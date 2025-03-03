6ce8c0a678fe62d9ab322c37e562b9453340f6a3
Dhinakaran <dhinaharanmohan79@gmail.com>
2025-03-03 11:25:58 +0530

    Changes made based on review

--- BST.rb
+++ BST.rb
@@ -10,6 +10,7 @@ class BST
   end
 
   def insert_node(data)
+    $file_handler.add(data)
     if @root_node.nil?
       @root_node = Node.new(data)
     else
@@ -18,14 +19,12 @@ class BST
       loop do
         if current_node.nil?
           current_node = Node.new(data)
-          $file_handler.add(data)
           break
         end
 
         if current_node.data < data
           if current_node.next_pointer.nil?
             current_node.next_pointer = Node.new(data)
-            $file_handler.add(data)
             break
           else
             current_node = current_node.next_pointer
@@ -36,41 +35,37 @@ class BST
         if current_node.data > data
           if current_node.prev_pointer.nil?
             current_node.prev_pointer = Node.new(data)
-            $file_handler.add(data)
             break
           else
             current_node = current_node.prev_pointer
             next
           end
         end
-        break
-      end
 
+        if current_node.data == data
+          puts "Element already exists"
+          break
+        end
+      end
     end
   end
 
   def inorder_traversal(temp_root = @root_node)
-    if temp_root.nil?
-      return
-    end
+    return if temp_root.nil?
     inorder_traversal(temp_root.prev_pointer)
     puts(temp_root.data)
     inorder_traversal(temp_root.next_pointer)
   end
 
   def preorder_traversal(temp_root = @root_node)
-    if temp_root.nil?
-      return
-    end
+    return if temp_root.nil?
     puts(temp_root.data)
     preorder_traversal(temp_root.prev_pointer)
     preorder_traversal(temp_root.next_pointer)
   end
 
   def postorder_traversal(temp_root = @root_node)
-    if temp_root.nil?
-      return
-    end
+    return if temp_root.nil?
     postorder_traversal(temp_root.prev_pointer)
     postorder_traversal(temp_root.next_pointer)
     puts(temp_root.data)
@@ -81,13 +76,10 @@ class BST
       puts "Tree is empty"
       return
     end
-
     max_node = @root_node
-
     while !max_node.next_pointer.nil?
       max_node = max_node.next_pointer
     end
-
     puts max_node.data
   end
 
@@ -96,13 +88,10 @@ class BST
       puts "Tree is empty"
       return
     end
-
     min_node = @root_node
-
     while !min_node.prev_pointer.nil?
       min_node = min_node.prev_pointer
     end
-
     puts min_node.data
   end
 
@@ -150,10 +139,7 @@ class BST
   end    
 
   def print_paths(root_node, track_path = [])
-    if root_node.nil?
-      return
-    end
-
+    return if root_node.nil?
     duplicate = track_path.dup
     duplicate.append(root_node.data)
     print_paths(root_node.prev_pointer, duplicate)
@@ -170,10 +156,7 @@ class BST
 
   def delete_node(root_node = @root_node, delete_element)
     $file_handler.delete?(delete_element)
-    if root_node.nil?
-      return root_node
-    end
-
+    return if root_node.nil?
     if root_node.data > delete_element
       root_node.prev_pointer = delete_node(root_node.prev_pointer, delete_element)
     elsif root_node.data < delete_element
@@ -208,8 +191,8 @@ object = BST.new()
 puts "Enter the file name you want to work with"
 file = gets.chomp
 
-if  File.exist?(file)
-  open_file = File.open(file, "r+")
+if File.exist?(file)
+  open_file = File.open(file, "r")
   if !File.empty?(file)
     open_file.each_line do |iterator|
       object.insert_node(iterator.to_i)
@@ -219,13 +202,17 @@ else
   open_file = File.new(file, "w+")
 end
 
+$file_handler.each do |iterator|
+  puts iterator
+end
+
 loop do
   puts "1.Insert\n2.Inorder\n3.Preorder\n4.Postorder\n5.Levelorder\n6.Search\n7.Max_Element\n"\
        "8.Min_Element\n9.Delete\n10.Print_All_Paths\n11.Quit\n"
-  input = gets.chomp.to_i
+  input = gets.to_i
   case input
   when 1
-    new_element = gets.chomp.to_i
+    new_element = gets.to_i
     object.insert_node(new_element)
     puts "---"
   when 2
@@ -242,7 +229,7 @@ loop do
     puts "---"
   when 6
     puts "Enter the value"
-    search_element = gets.chomp.to_i
+    search_element = gets.to_i
     object.search_element(search_element)
     puts "---"
   when 7
@@ -252,7 +239,7 @@ loop do
     object.find_minimum
     puts "---"
   when 9
-    del_node = gets.chomp.to_i
+    del_node = gets.to_i
     object.delete_node(object.root_node, del_node)
     puts "---"
   when 10
